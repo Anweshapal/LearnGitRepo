31172f9cc05468b65858d49633031e1fbdbe5a4f
Anwesha Pal <anweshapal121@gmail.com>
2021-09-30 12:19:27 +0530

    added message Sum.java

--- Sum.java
+++ Sum.java
@@ -1,6 +1,6 @@
 public classSum{
     public static void main(String[] args) {
         int a=2,int b=3;
-        system.out.println(a+b);
+        system.out.println("the sum is : "+(a+b));
     }
 }
